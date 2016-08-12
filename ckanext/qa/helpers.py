e1f44a93f9b7431d922c84cb571429b29e8a79f2
David Read <david.read@hackneyworkshop.com>
2016-08-12 13:14:35 +0100

    Fix for when qa extra value has been converted to a string, during harvesting perhaps.

--- ckanext/qa/helpers.py
+++ ckanext/qa/helpers.py
@@ -6,6 +6,9 @@ def qa_openness_stars_resource_html(resource):
     qa = resource.get('qa')
     if not qa:
         return tk.literal('<!-- No qa info for this resource -->')
+    if not isinstance(qa, dict):
+        return tk.literal('<!-- QA info was of the wrong type -->')
+
     # Take a copy of the qa dict, because weirdly the renderer appears to add
     # keys to it like _ and app_globals. This is bad because when it comes to
     # render the debug in the footer those extra keys take about 30s to render,
@@ -20,6 +23,8 @@ def qa_openness_stars_dataset_html(dataset):
     qa = dataset.get('qa')
     if not qa:
         return tk.literal('<!-- No qa info for this dataset -->')
+    if not isinstance(qa, dict):
+        return tk.literal('<!-- QA info was of the wrong type -->')
     extra_vars = copy.deepcopy(qa)
     return tk.literal(
         tk.render('qa/openness_stars_brief.html',
