cf9bc028b97975dc255998cdf6e19b8e3bef09bc
antuarc <carl.antuar@smartservice.qld.gov.au>
2021-02-12 12:27:27 +1000

    sync mock server with the one from ckanext-archiver, including fixing text vs binary typing

--- ckanext/qa/tests/mock_remote_server.py
+++ ckanext/qa/tests/mock_remote_server.py
@@ -97,7 +97,8 @@ class MockEchoTestServer(MockHTTPServer):
         a 500 error response: 'http://localhost/?status=500'
 
         a 200 OK response, returning the function's docstring:
-         'http://localhost/?status=200;content-type=text/plain;content_var=ckan.tests.lib.test_package_search:test_wsgi_app.__doc__'
+        'http://localhost/?status=200;content-type=text/plain;content_var
+        =ckan.tests.lib.test_package_search:test_wsgi_app.__doc__'
 
     To specify content, use:
 
@@ -114,10 +115,16 @@ class MockEchoTestServer(MockHTTPServer):
         if 'content_var' in request.str_params:
             content = request.str_params.get('content_var')
             content = self.get_content(content)
+        elif 'content_long' in request.str_params:
+            content = '*' * 1000001
         else:
             content = request.str_params.get('content', '')
+        if 'method' in request.str_params \
+                and request.method.lower() != request.str_params['method'].lower():
+            content = ''
+            status = 405
 
-        if isinstance(content, six.string_types):
+        if isinstance(content, six.text_type):
             raise TypeError("Expected raw byte string for content")
 
         headers = [
@@ -125,8 +132,11 @@ class MockEchoTestServer(MockHTTPServer):
             for item in request.str_params.items()
             if item[0] not in ('content', 'status')
         ]
-        if content:
-            headers += [('Content-Length', str(len(content)))]
+        if 'length' in request.str_params:
+            cl = request.str_params.get('length')
+            headers += [('Content-Length', cl)]
+        elif content and 'no-content-length' not in request.str_params:
+            headers += [('Content-Length', six.binary_type(len(content)))]
         start_response(
             '%d %s' % (status, responses[status]),
             headers
