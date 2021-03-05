7fcf5096d1e0e1b6673797457e3179a6ba126453
antuarc <carl.antuar@smartservice.qld.gov.au>
2021-03-05 14:14:37 +1000

    move requirements to a separate file instead of setup.py

--- setup.py
+++ setup.py
@@ -17,20 +17,16 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'ckanext-archiver>=2.0',
-        'ckanext-report',
-        'SQLAlchemy>=0.6.6',
-        'requests',
-        'xlrd>=0.8.0',
-        'messytables>=0.8',
-        'python-magic>=0.4',
-        'progressbar',
-        'six>=1.9' # until messytables->html5lib releases https://github.com/html5lib/html5lib-python/pull/301
+      # CKAN extensions should not list dependencies here, but in a separate
+      # ``requirements.txt`` file.
+      #
+      # http://docs.ckan.org/en/latest/extensions/best-practices.html#add-third-party-libraries-to-requirements-txt
     ],
     tests_require=[
-        'nose',
-        'mock',
-        'flask'
+      # CKAN extensions should not list dependencies here, but in a separate
+      # ``dev-requirements.txt`` file.
+      #
+      # http://docs.ckan.org/en/latest/extensions/best-practices.html#add-third-party-libraries-to-requirements-txt
     ],
     entry_points='''
     [paste.paster_command]
